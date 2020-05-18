344be8996c96a99dfded625c3d39ba5f3d9285f3
Divsatwork <divyansh.chopra31@gmail.com>
2020-05-18 17:48:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'flaskinator',         # How you named your package folder (MyLib)
   packages = ['flaskinator'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quick library to generate ready to use APIs for Flask environment',   # Give a short description about your library
   author = 'Divs',                   # Type in your name
   url = 'https://github.com/Divsatwork/flaskinator',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Divsatwork/flaskinator/archive/v_0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Divsatwork/flaskinator/archive/v_0.2.tar.gz',    # I explain this later on
   keywords = ['Flask', 'Ready to use', 'API'],   # Keywords that define your package best
   install_requires=[ ], #no dependencies
   classifiers=[
