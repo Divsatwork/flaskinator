7aad4c02995721d1a4364518040ee7804b15a96d
Divsatwork <divyansh.chopra31@gmail.com>
2020-05-18 22:22:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, Extension
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-  name = 'flaskinator',         # How you named your package folder (MyLib)
-  packages = ['flaskinator'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Quick library to generate ready to use APIs for Flask environment',   # Give a short description about your library
-  author = 'Divs',                   # Type in your name
-  url = 'https://github.com/Divsatwork/flaskinator',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Divsatwork/flaskinator/archive/v_0.2.tar.gz',    # I explain this later on
-  keywords = ['Flask', 'Ready to use', 'API'],   # Keywords that define your package best
+  name = 'flaskinator',         
+  packages = ['flaskinator'],   
+  version = '0.1.3',      
+  license='MIT',        
+  description = 'Quick library to generate ready to use APIs for Flask environment',
+  author = 'Divs',
+  url = 'https://github.com/Divsatwork/flaskinator',
+  download_url = 'https://github.com/Divsatwork/flaskinator/archive/v_0.1.2.tar.gz',
+  keywords = ['Flask', 'Ready to use', 'API'],
   install_requires=[ ], #no dependencies
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -20,4 +28,6 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
+  long_description=long_description,
+  long_description_content_type='text/markdown'
 )
